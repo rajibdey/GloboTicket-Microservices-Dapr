da6b71abdbee011c36d3c16a69e11989031fc62a
Rajib Dey <v-RDey@simsupplychain.com>
2021-06-29 01:32:34 +0530

    added packages gitignore

---
+++ src/Packages/Extensions/GloboTicket.Packages.Extensions/HostEnvironmentExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace GloboTicket.Packages.Extensions
+{
+    public static class HostEnvironmentExtensions
+    {
+        public static bool IsDocker(this IHostEnvironment hostEnvironment) =>
+            "docker" == hostEnvironment.EnvironmentName.ToLower();
+        public static bool IsTest(this IHostEnvironment hostEnvironment) =>
+            "test" == hostEnvironment.EnvironmentName.ToLower();
+    }
+}
